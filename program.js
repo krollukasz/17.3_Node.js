d25a50582003dee3338176ece5902d2e9a2b3179
Łukasz Król <lionel@o2.pl>
2019-01-12 18:11:05 +0100

    Add instruction to show node.js version and language

---
+++ program.js
@@ -0,0 +1,29 @@
+process.stdin.setEncoding("utf-8"); // ustawienie odpowiedniego enkodowania, odczytanie wartości jako string kodowany z utf-8
+
+process.stdin.on("readable", function() { // ustawienie nasłuchiwania na zdarzenia odczytu za pomocą .on
+  var input = process.stdin.read(); // metoda .read() ma odczytać, co wpisał użytkownik
+  if (input !== null) { // instrukcja warunkowa, która sprawdzająca, czy na wejściu podano jakąś wartość
+    
+    var instruction = input.toString().trim();
+      switch (instruction) {
+        case "/exit":
+          process.stdout.write("Quitting app!\n");
+          process.exit();
+          break;
+
+        case "/version":
+          process.stdout.write("Your version of node.js is: " + process.versions.node + "\n");
+          process.exit();
+          break;
+
+        case "/language":
+          process.stdout.write("The language of your operating system is: " + process.env.lang + "\n");
+          process.exit();
+          break;
+
+        default:
+          process.stderr.write("Wrong instruction!\n");
+      }
+    process.stdin.write(input);
+  }
+});
